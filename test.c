86729392af392eb6de6d04e7895eb97619c5f799
tivo <tivo@tivo-odc.(none)>
2019-04-23 12:46:23 +0530

    first submit

--- test.c
+++ test.c
@@ -1,1 +1,19 @@
-#include<Mantoo>
+#include<stdio.h>
+#include<stdlib.h>
+
+void test1()
+{
+    const int NUM_HEIGHTS=3;
+    int i;
+    int *heights = malloc(NUM_HEIGHTS * sizeof(int));
+    for (i = 0; i < NUM_HEIGHTS; i++)
+    {
+        heights[i] = i * i;
+    }
+    free(heights);
+    printf("Welcome to test session\n");
+}
+int main()
+{
+    test1();
+}
